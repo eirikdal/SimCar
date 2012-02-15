d586090026232fc76da75d3bda8a2d4098ac4b29
Eirik Daleng Haukedal <eirik.haukedal@gmail.com>
2012-02-15 10:23:19 +0100

    test

--- SimCar/SimCar.fs
+++ SimCar/SimCar.fs
@@ -6,11 +6,15 @@ open System.IO
 open Models
 open Message
 open PHEV
-//open FileManager
+open ComManager
+open FileManager
 
 [<EntryPoint>]
 let main args = 
 //    let phevs = list_of_phevs()
+    let comManager = new ComManager()
 
+    let phev_agents = list_of_phevs()
+    Seq.iter (fun phev -> comManager.add_agent(phev_agent phev))
     ignore(Console.ReadKey())
     0
