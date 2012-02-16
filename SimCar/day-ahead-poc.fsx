81ec9fd1bd5692fd5ddfb04332c59313b090b0c1
Eirik Daleng Haukedal <eirikdal@stud.ntnu.no>
2012-02-16 15:58:19 +0100

    few fixes

--- SimCar/day-ahead-poc.fsx
+++ SimCar/day-ahead-poc.fsx
@@ -13,29 +13,41 @@ let i = Array.findIndex (fun w' -> w = w') D
 let mutable k = i + 1
 let mutable S = 0.0
 
-let reduce k op neg = 
-    let ik = i+k
-    let ik' = i-k
-
-    if ik > 0 && w > D.[ik] then
-        S <- op S (delta D.[ik])
-        D.[ik] <- neg D.[ik] (delta D.[ik])
-    if ik' > 0 && w < D.[ik'] then 
-        S <- op S (delta D.[ik'])
-        D.[ik'] <- neg D.[ik'] (delta D.[ik'])        
-
-let rec build k = 
-    if S >= 0.0 then
-        
-        build (k+1)
-
 let rec scan k = 
-    if w < D.[i-k] && w < D.[i+k] then 
-        build (k+1)
-    else
-        if w > D.[i+k] || w > D.[i-k] then
-            reduce k (+) (-)
-        else if w < D.[k] then 
-            reduce k (-) (+)
-             
+    let ik = i + k
+    let ik' = i - k 
+
+    if S > 0.0 then
+        if ik <= D.Length then
+            S <- S - delta D.[ik]
+        if ik' >= 0 then
+            S <- S - delta D.[ik']
+
         scan (k+1)
+
+//let reduce k op neg = 
+//    let ik = i+k
+//    let ik' = i-k
+//
+//    if ik > 0 && w > D.[ik] then
+//        S <- op S (delta D.[ik])
+//        D.[ik] <- neg D.[ik] (delta D.[ik])
+//    if ik' > 0 && w < D.[ik'] then 
+//        S <- op S (delta D.[ik'])
+//        D.[ik'] <- neg D.[ik'] (delta D.[ik'])        
+//
+//let rec build k = 
+//    if S >= 0.0 then
+//        
+//        build (k+1)
+//
+//let rec scan k = 
+//    if w < D.[i-k] && w < D.[i+k] then 
+//        build (k+1)
+//    else
+//        if w > D.[i+k] || w > D.[i-k] then
+//            reduce k (+) (-)
+//        else if w < D.[k] then 
+//            reduce k (-) (+)
+//             
+//        scan (k+1)
