921ced3612418603d4de011d69b2b8759aec3ed0
Eirik Daleng Haukedal <eirikdal@stud.ntnu.no>
2012-04-03 14:59:12 +0200

    Holistic

--- SimCar/DayaheadSwarm.fs
+++ SimCar/DayaheadSwarm.fs
@@ -42,7 +42,7 @@ let mutable agg_dist' =
 let mutable agg_dist : float array = (agg_dist'.Clone() :?> float array)
 
 type Message = 
-    | Init of int * int
+    | Init of int 
     | Moved of int * int
     | Fill of float
     | FillQuery of float * int
@@ -54,119 +54,56 @@ type Message =
     | Failed of int
     | Exit
 
-let num_agents = 20
-let theta = 0.99
+let num_agents = 95
+let theta = 0.999
 let sum_agg_dist = agg_dist |> Array.sum
 
 let rate = 1.25
 
 module Swarm = 
     let init (realtime : float array) = 
+        let rand = new System.Random()
         let rec ants = 
             [for i in 0 .. num_agents do 
                 yield Agent<Message>.Start(fun agent ->
                 let rec loop n pos = agent.Scan((function
-                    | Init(pos, inertia) -> 
-//                        printfn "Agent %d initialized with position %d" n pos
-                
-                        Some( async { return! moving n pos inertia } )
+                    | Init(pos) -> 
+                        Some( async { return! filling n pos } )
                     | _ -> None), 10000)
-                and moving n pos inertia = async {
-                    if (pos+inertia) < realtime.Length && (pos+inertia) > 0 && realtime.[pos+inertia] < realtime.[pos] then 
-                        return! moving n (pos+inertia) inertia
-                    else
-                        supervisor.Post(Moved(n, pos))
-                        return! waiting n pos inertia }
-                and waiting n pos inertia = 
-                    agent.Scan((function 
-                        | Occupied -> 
-                            if (pos+inertia) >= realtime.Length || (pos+inertia) < 0 then
-                                supervisor.Post(Failed(n))
-                                Some(async { return! failed n })
-                            else
-                                Some(async { return! moving n (pos+inertia) inertia })
-                        | Accepted -> 
-                            Some(async { return! filling n pos inertia })
-                        | _ -> None),10000)
-                and filling id pos inertia = 
+                and filling id pos = 
                     agent.Scan((function
                         | FillQuery(rate, pos') -> 
                             let distance = theta ** (float <| abs(pos-pos'))
                             supervisor.Post(Utility(id, distance*(1.0 / realtime.[pos])))
-                            Some(async { return! filling id pos inertia })
+                            Some(async { return! filling id pos })
                         | Fill(rate) ->
-//                            printfn "Updating realtime with %f at %d" rate pos
                             realtime.[pos] <- realtime.[pos] + rate
                             syncContext.RaiseDelegateEvent dayaheadStep (realtime.Clone())
 
-//                            let pos', inertia' = 
-//                                if (pos+inertia) < realtime.Length && (pos+inertia > 0) && realtime.[pos+inertia] < realtime.[pos] then
-//                                    pos+inertia, inertia
-//                                else
-//                                    pos, (inertia*(-1))
-                            
-//                            reply.Reply(Moved(id, pos'))
-                            Some(async { return! moving id pos inertia })
+                            Some(async { return! filling id pos })
                         | Exit -> Some( async { () } )),10000)
-                and failed id = 
-                    agent.Scan((function 
-                        | Exit -> Some(async { () })
-                        | FillQuery(_,_) -> 
-                            supervisor.Post(Utility(id, -1.0))
-                            Some(async { return! failed id })
-                        | Init(_,_) ->
-                            Some(async { return! failed id })
-                        | _ -> None), 10000)
                 loop i 0)]
         and supervisor : Agent<Message> = 
             let agent_pos = Array.init (ants.Length) (fun _ -> -1)
     
             let peak = realtime |> Array.max
             let init_pos = realtime |> Array.findIndex (fun x -> x = peak)
-    
+//            let rand = new System.Random()
             Agent<Message>.Start(fun agent ->
                 ants |> List.iteri (fun i ant -> 
                     let inertia = if i % 2 = 0 then 1 else -1
-                    ant.Post(Init(init_pos, inertia)))
+                    ant.Post(Init(i)))
                 
-                let rec loop() = 
-                    let all_in_pos agent_pos = 
-                        let all_in_pos = agent_pos |> Array.forall (fun p -> p = (int infinity) || p >= 0)
-
-                        if all_in_pos then 
-//                            printfn "All agents in position" 
-                            Some(async { return! filling agg_dist'.[0] 0 })
-                        else
-                            Some(async { return! loop() }) 
-                    agent.Scan(function
-                    | Moved(ag, pos) ->
-                        let d = agent_pos |> Array.tryFindIndex (fun p -> p <> (int infinity) && abs(pos-p) < 5)
-
-                        match d with
-                        | Some _ -> 
-                            ants.[ag].Post(Occupied)
-                        | None -> 
-                            agent_pos.[ag] <- pos
-                            ants.[ag].Post(Accepted)
-
-                        syncContext.RaiseDelegateEvent dayaheadAnt (agent_pos.Clone() :?> int array, realtime.Clone() :?> float array)
-
-                        all_in_pos agent_pos
-                    | Failed(ag) -> 
-                        agent_pos.[ag] <- int infinity
-
-                        all_in_pos agent_pos
-                    | _ -> None)
+                let rec loop() = async {
+                    return! filling agg_dist'.[0] 0 }
                 and filling remaining pos = async { 
                     if remaining > rate then
                         let rate' = if remaining > rate then rate else rate-remaining 
                         ants |> List.iter (fun ant -> ant.Post(FillQuery(rate', pos)))
-//                        printfn "Sending fillqueries for %d" pos
                         return! waiting remaining [] pos
                     else if (pos+1) < agg_dist'.Length then
                         return! filling (agg_dist'.[pos+1]) (pos+1)
                     else 
-//                        printfn "Dayahead complete"
                         ants |> List.iter (fun ant -> ant.Post(Exit))
                 
                         return! idle() }
@@ -178,28 +115,11 @@ module Swarm =
                         syncContext.RaiseDelegateEvent dayaheadExpected (agg_dist'.Clone() :?> float array)
                         ants.[id].Post(Fill(rate'))
 
-                        async { return! prep (remaining-rate') pos }
+                        async { return! filling (remaining-rate') pos }
                     else
                         agent.Scan((function 
                             | Utility(_,_) as msg -> Some( async { return! waiting remaining (msg :: agent_responses) pos } )
                             | _ -> None), 10000)
-                and prep remaining pos = agent.Scan((function 
-                    | Moved(ag, pos') ->
-                        let d = agent_pos |> Array.tryFindIndex (fun p -> p <> (int infinity) && p <> agent_pos.[ag] && abs(pos'-p) < 5)
-
-                        match d with
-                        | Some _ -> 
-                            ants.[ag].Post(Occupied)
-                            Some(async { return! prep remaining pos })
-                        | None -> 
-                            agent_pos.[ag] <- pos'
-                            ants.[ag].Post(Accepted)
-
-                            syncContext.RaiseDelegateEvent dayaheadAnt (agent_pos.Clone() :?> int array, realtime.Clone() :?> float array)
-
-                            Some(async { return! filling remaining pos })
-                    | Failed(ag) -> agent_pos.[ag] <- int infinity; Some(async { return! filling remaining pos })
-                    | _ -> None),10000)
                 and idle() = agent.Scan((function 
                     | RequestChart(reply) -> reply.Reply(Chart(realtime)); Some(async {()})
                     | Exit -> Some(async { () })
