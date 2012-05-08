8496ff2616fc7b7e9d16d8c7e7018ee447098544
Eirik Daleng Haukedal <eirik.haukedal@gmail.com>
2012-05-08 14:35:35 +0200

    fetch

--- SimCar/parse_realtime_data.fsx
+++ SimCar/parse_realtime_data.fsx
@@ -12,7 +12,7 @@ open FileManager
 
 #nowarn "25"
 
-let powerprofile_file = "C:\\SimCar\\SimCar\\data\\raw.txt"
+let powerprofile_file = "C:\\SimCar\\SimCar\\data\\buskerud.txt"
 let profile_file = "C:\\SimCar\\SimCar\\data\\powerprofiles.txt"
 let data_folder = "C:\\SimCar\\SimCar\\data\\interpol\\"
 
@@ -43,15 +43,16 @@ let rec parse_powerprofiles stream (values : float list) initdate days customer 
         match h.Split([|' ';'\t'|], StringSplitOptions.RemoveEmptyEntries) with
         | [|cust;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_|] when cust = "Customer" ->
             parse_powerprofiles t values initdate days customer
-        | [|cust;date;est;q1;q2;q3;q4;q5;q6;q7;q8;q9;q10;q11;q12;q13;q14;q15;q16;q17;q18;q19;q20;q21;q22;q23;q24|] when days = 0 ->
-            IO.write_doubles (data_folder + (sprintf "%s.dat" cust)) (List.rev values)
-            parse_powerprofiles t [] initdate (days-1) cust
+        | [|cust;date;est;q1;q2;q3;q4;q5;q6;q7;q8;q9;q10;q11;q12;q13;q14;q15;q16;q17;q18;q19;q20;q21;q22;q23;q24|] when days <= 0 && cust = customer ->
+//            IO.write_doubles (data_folder + (sprintf "%s.dat" cust)) (List.rev values)
+            parse_powerprofiles t values initdate (days-1) cust
         | [|cust;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_;_|] when days < 0 && customer = cust ->
             parse_powerprofiles t values initdate days cust
-        | [|cust;date;est;q1;q2;q3;q4;q5;q6;q7;q8;q9;q10;q11;q12;q13;q14;q15;q16;q17;q18;q19;q20;q21;q22;q23;q24|] when days < 0 && cust <> customer ->
-            let hours = q1::q2::q3::q4::q5::q6::q7::q8::q9::q10::q11::q12::q13::q14::q15::q16::q17::q18::q19::q20::q21::q22::q23::q24::[]
-            let (day : float list) = [for f in make_day hours do yield f]
-            parse_powerprofiles t ((List.rev day) @ values) date 30 cust
+        | [|cust;date;est;q1;q2;q3;q4;q5;q6;q7;q8;q9;q10;q11;q12;q13;q14;q15;q16;q17;q18;q19;q20;q21;q22;q23;q24|] when days <= 0 && cust <> customer ->
+//            let hours = q1::q2::q3::q4::q5::q6::q7::q8::q9::q10::q11::q12::q13::q14::q15::q16::q17::q18::q19::q20::q21::q22::q23::q24::[]
+//            let (day : float list) = [for f in make_day hours do yield f]
+            IO.write_doubles (data_folder + (sprintf "%s.dat" customer)) (List.rev values)
+            parse_powerprofiles (h::t) [] date 30 cust
         | [|cust;date;est;q1;q2;q3;q4;q5;q6;q7;q8;q9;q10;q11;q12;q13;q14;q15;q16;q17;q18;q19;q20;q21;q22;q23;q24|] when cust <> "Customer" ->
             let hours = q1::q2::q3::q4::q5::q6::q7::q8::q9::q10::q11::q12::q13::q14::q15::q16::q17::q18::q19::q20::q21::q22::q23::q24::[]
             let (day : float list) = make_day hours
@@ -65,4 +66,4 @@ let powerprofiles =
     let mutable rest = []
     let stream = List.ofSeq read_file
     
-    parse_powerprofiles stream [] (new DateTime(2006,01,01).ToString()) "-1"
+    parse_powerprofiles stream [] ((new DateTime(2006,01,01)).ToString()) 30 "-1"
