8d28b192d8518b8e43c02fcb49b5d4f7e802c212
Eirik Daleng Haukedal <eirik.haukedal@gmail.com>
2012-06-19 10:08:23 +0200

    mono?

---
+++ SimApp/Program.fs
@@ -0,0 +1,17 @@
+﻿module SimCon
+
+open SimCar
+
+let simCar = new Sim.SimCar(96)
+
+let test2 str = 
+    printfn "%s" str |> ignore
+
+simCar.RegisterProgressEvent(test2)
+simCar.RegisterDebugEvent(test2)
+simCar.Scheduler <- None
+simCar.Contribution <- None
+simCar.Method <- None
+System.Threading.Thread.Sleep(5000)
+simCar.ComputeDayahead()
+simCar.Run(sprintf "%s" <| System.String.Format("{0:dd.MM.HH.mm}", System.DateTime.Now))
